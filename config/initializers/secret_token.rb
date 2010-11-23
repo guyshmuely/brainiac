fc2cee9571497f57215c2ba992253f711fcce7ff
Guy Shmuely <g.shmuely@gmail.com>
2010-11-23 23:07:52 +0200

    new app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Brainiac::Application.config.secret_token = 'ef912d66d9e900ea848056e38cc09ef51d858abfb3901b1ccee7b5edb275782ca670c81a8f3fab6f2636bffd86bc8d3a3fc60f08ba4443f5dbc628f4e23b49e8'
