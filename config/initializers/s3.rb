7b57ad872c4dc6262d935e4cbaf35ac89e9e17ef
Guy Shmuely <g.shmuely@gmail.com>
2010-12-12 18:04:21 +0200

    deleted all.js

--- config/initializers/s3.rb
+++ config/initializers/s3.rb
@@ -3,8 +3,8 @@
 S3 = {}
 if File.exists?("#{Rails.root}/config/s3.yml")
   s3_config = YAML.load_file("#{Rails.root}/config/s3.yml")
-  S3[:key] = s3_config[Rails.env]['key']
-  S3[:secret] = s3_config[Rails.env]['secret']
+  S3[:key] = s3_config[Rails.env]['access_key_id']
+  S3[:secret] = s3_config[Rails.env]['secret_access_key']
   S3[:bucket] = s3_config[Rails.env]['bucket']
 else
   S3[:key] = ENV['S3_KEY']
