9d8bfeda48c29c98b7fa4de6f7a9e7090f20ec1b
Guy Shmuely <g.shmuely@gmail.com>
2011-04-01 22:34:34 +0300

    added :host parameter to default url options at production.rb for mailing purpose

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -47,4 +47,7 @@ Brainiac::Application.configure do
 
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
+
+  config.action_mailer.default_url_options = { :host => 'brainiac.heroku.com' }
+
 end
