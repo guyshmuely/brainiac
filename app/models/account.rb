af86a4378f3d7cf3a74ffb36ce7fb24f879effc4
Guy Shmuely <g.shmuely@gmail.com>
2011-04-11 23:21:18 +0300

    changed devise encrypt module to fit latest devise release

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,8 +1,8 @@
 class Account < ActiveRecord::Base
   # Include default devise modules. Others available are:
   devise :database_authenticatable, :registerable, :confirmable,
-         :omniauthable, :lockable, :timeoutable, :encryptable , :token_authenticatable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :omniauthable, :lockable, :timeoutable, :encryptable, :token_authenticatable,
+         :recoverable, :rememberable, :trackable, :validatable, :encryptor => :sha512
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me
